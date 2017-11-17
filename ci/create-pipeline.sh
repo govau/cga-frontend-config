fabf67058fab98aa9bfc48ec2c22d9e1a7fe5cc6
Brendan Gordon <brendan.gordon@digital.gov.au>
2017-11-17 11:44:07 +1100

    Deploy the release to the routers

--- ci/create-pipeline.sh
+++ ci/create-pipeline.sh
@@ -10,7 +10,11 @@ fly validate-pipeline --config pipeline.yml
 
 fly --target ${TARGET} set-pipeline --config pipeline.yml --pipeline $PIPELINE -n -l credentials.yml
 
-for resource in  ops.git this.git certs-g.s3 certs-d.s3; do
+for resource in  ops.git this.git \
+    certs-b.s3 release-b.s3 \
+    certs-d.s3 release-d.s3 \
+    certs-g.s3 release-g.s3 \
+    certs-y.s3 release-y.s3; do
   fly -t ${TARGET} check-resource --resource $PIPELINE/$resource
 done
 
